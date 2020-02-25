55e1940e443c19dc41d8e9c0731c12ce78e223f8
CiprianaM <cipriana2001@gmail.com>
2020-02-25 11:51:36 +0100

    added first action, modified store by dispatching an action

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import rootReducer from './reducers';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+import { addCharacterById } from './actions';
+
 const store = createStore(rootReducer);
-console.log(store.getState());
+console.log('store.getState()', store.getState());
+store.subscribe(()=> console.log('store', store.getState()));
+store.dispatch(addCharacterById(9))
 
 ReactDOM.render (
   <Provider store={store}>
